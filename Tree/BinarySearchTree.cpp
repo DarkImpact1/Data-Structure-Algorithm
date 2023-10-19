9f190f34982fa53d16743a6506a133606dac97c7
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2023-10-20 01:18:42 +0530

    Update BinarySearchTree.cpp

--- Tree/BinarySearchTree.cpp
+++ Tree/BinarySearchTree.cpp
@@ -25,6 +25,8 @@ struct Node
     Node *right_child;
 };
 Node *root = NULL;
+
+// Inserting value in BST
 Node* Insert(Node *root, int val)
 {
     if(root == NULL)
@@ -39,17 +41,18 @@ Node* Insert(Node *root, int val)
     	cout<<"Duplicates are not allowed in BST : "<<endl;
     	return root;
 	}
-    else if(root->data >val )
+    else if(root->data >val ) // insert value in left BST
     {
         root->left_child = Insert(root->left_child, val );
     }
-    else 
+    else // Insert value in right BST
     {
         root->right_child = Insert(root->right_child, val);
     }
 
 }
-void traverse_inorder(Node *root)// left - root - child
+void traverse_inorder(Node *root)// left - root - child  O(h)
+
 {
 
 	if(root == 0)
@@ -60,7 +63,8 @@ void traverse_inorder(Node *root)// left - root - child
 	cout << root->data << " ";
 	traverse_inorder(root->right_child);
 }
-void DeallocateMemory(Node* root) {
+void DeallocateMemory(Node* root) // O(h)
+{
     if (root == NULL) {
         return;
     }
@@ -68,6 +72,69 @@ void DeallocateMemory(Node* root) {
     DeallocateMemory(root->right_child);
     delete root; // Deallocate memory for the current node
     
+}
+bool Search(Node *root, int key)// O(h)
+{
+	if(root == NULL)
+	{
+		return false;
+	}
+	else if(key > root->data)// search in right BST
+	{
+		Search(root->right_child, key);
+	}
+	else if(key == root->data)
+	{
+		return true;
+	}
+	else//search in left BST
+	{
+		Search(root->left_child,key);
+	}
+}
+// to search Inorder successor for the 3rd case of deletion
+Node* InorderSuccessor(Node *root)
+{
+	while(root->left_child != NULL)
+	{
+		root = root->left_child;
+	}
+	return root;
+}
+Node* Delete(Node *root, int value)
+{
+	if(value > root->data)// delete from right BST
+	{
+		root->right_child = Delete(root->right_child,value);
+	}
+	else if (value < root ->data)// delete from left BST
+	{
+		root->left_child = Delete(root->left_child,value);
+	}
+	else
+	{
+		// when it's a leaf node 
+		if(root->left_child == NULL && root->right_child == NULL)
+		{
+			return NULL;
+		}
+		// Case 1: when root->left == NULL
+		else if(root->left_child == NULL)
+		{
+			return root->right_child;
+		}
+		else if (root->right_child == NULL)
+		{
+			return root->left_child;
+		}
+		else
+		{
+			Node *inordersuc = InorderSuccessor(root->right_child);
+			root->data = inordersuc->data;
+			root->right_child = Delete(root->right_child, inordersuc->data);
+		}
+		return root;
+	}
 }
 int main()
 {
@@ -85,6 +152,15 @@ int main()
     }
     traverse_inorder(root);
     // To delete the memory after occupied by dynamically allocation
+    int key;
+    cout<<"\nEnter the key you want to delete/Search in BST : ";cin>>key;
+    if(Search(root,key)) 
+	{
+		Delete(root,key);
+	}
+    else cout<<"Key not found : "<<endl;
+    traverse_inorder(root);
+    cout<<endl;
     DeallocateMemory(root);
     cout<<"\nDeallocated successfully : ";
 }
