2e7030a8cf3a6c1a52b04210bea60865bca5919f
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-20 17:00:34 +0530

    pattern

---
+++ patterns/pattern7.cpp
@@ -0,0 +1,34 @@
+/*
+pattern :
+                *********
+                 *******
+                  *****
+                   ***
+                    *
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void pattern(int n){
+    for(int i=0;i<n;i++){
+        //for space
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        // for *
+        for(int j=0; j<2*(n-i-1)+1;j++){
+            cout<<"*";
+        }
+        // for space 
+        for(int j=0; j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+int main(){
+    cout<<"Pattern :"<<endl;
+    pattern(5);
+}
