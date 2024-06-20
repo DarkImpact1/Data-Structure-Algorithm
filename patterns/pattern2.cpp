586430ba5efdacf2ba962ab50c1fa37cfb2273f3
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-20 16:28:17 +0530

    adding pattern2

---
+++ patterns/pattern2.cpp
@@ -0,0 +1,21 @@
+/*
+pattern1 : 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void pattern1(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
+int main(){
+    cout<<"Pattern 2 :"<<endl;
+    int n;
+    cin>>n;
+    pattern1(n);
+}
