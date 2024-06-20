7160e59ddbdd726a491a69ad7cb9b61bc4a2353e
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-20 17:36:47 +0530

    pattern

--- patterns/pattern12.cpp
+++ patterns/pattern12.cpp
@@ -1,19 +1,21 @@
 /*
-pattern :
-                *
-                **
-                ***
-                ****
-                ***** 
+Pattern :
+                1
+                2 3
+                4 5 6
+                7 8 9 10
+                11 12 13 14 15 
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 void pattern(int n){
+    int num = 1;
     for(int i=0;i<n;i++){
         for(int j=0;j<=i;j++){
-            cout<<"*";
-        }
+            cout<<num<<" ";
+            num++;
+            }
         cout<<endl;
     }
 
