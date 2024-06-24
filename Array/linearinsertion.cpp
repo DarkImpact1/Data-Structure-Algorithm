00b86d02a67f219d6ce49363ed9536487707d4f8
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-24 19:00:23 +0530

    insertion in an array

--- Array/linearinsertion.cpp
+++ Array/linearinsertion.cpp
@@ -1,14 +1,17 @@
-#inlude<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main()
-{
-	int size;
-	cout<<"Enter the size of an array : ";cin>>size;
-	int arr[size];
-	for(int i=0; i<size; i++)
-	{
-		cin>>arr[i];
+// insert zero at given index 
+void insert(int arr[], int pos, int n){
+	for(int i = n-1; i >= pos; i--){
+		arr[i] = arr[i-1];
 	}
-	int pos;
+	arr[pos-1] = 0;
+}
+int main() {
+    int arr[6] = {10,3,1,5,9};
+    insert(arr,3,6);
+    for(int i=0;i<6;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
 }
