7429498e925217dc4d34ad2757efa5f57d1f44c0
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-26 18:02:25 +0530

    remove duplicates

--- Array/removeduplicates.cpp
+++ Array/removeduplicates.cpp
@@ -7,7 +7,7 @@ Problem : Remove duplicates from the sorted order array and return total number 
 using namespace std;
 
 
-
+// two pointer approach
 int removeDuplicates(vector<int>& nums) {
         int i=0;
         for(int j = 1; j< nums.size(); j++){
@@ -18,6 +18,17 @@ int removeDuplicates(vector<int>& nums) {
         }
         return i+1;
     }
+// Another method
+int removeDuplicates2(vector<int> &nums){
+    int unique = 1;
+    for(int i = 1;i<nums.size(); i++){
+        if(nums[i] != nums[i-1]){
+            nums[unique] = nums[i];
+            unique++;
+        }
+    }
+    return unique;
+}
 int main() {
     vector<int> vec  = {0,1,1,1,1,2,2,3,3,4,5};
     int n = removeDuplicates(vec);
