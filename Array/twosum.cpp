c57fecd863ea33058c0ac5b6b27a9d3e916c87f7
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-27 16:55:21 +0530

    adding two sum

--- Array/twosum.cpp
+++ Array/twosum.cpp
@@ -16,7 +16,14 @@ string twoSumExist(int n, vector<int> vec, int k){
     return "No";
 }
 
-
+// two pointer approach
+string sumExist(int n, vector<int> nums, int k){
+    int left = 0, right = n-1;
+    sort(nums.begin(), nums.end());
+    while(left < right){
+        
+    }
+}
 int main(){
     vector<int> vec = {2,4,3,5,6,8};
     cout<<twoSumExist(6,vec,12);
