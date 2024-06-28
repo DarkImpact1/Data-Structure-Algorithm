7836987b914f74f24f41114619a4a70586157f09
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-28 18:45:40 +0530

    brute sol for max subarray

--- Array/max_subarray.cpp
+++ Array/max_subarray.cpp
@@ -1,9 +1,29 @@
-#include <bits/stdc++.h>
+// Max subarray problem
+#include<bits/stdc++.h>
 using namespace std;
 
+ // time complexity O(n**3)
+int brute_max_arr(vector<int> nums){
+    int n=nums.size();
+    // because array will contain atleast one element
+    int m=nums[0];
+    for(int i=0;i<n;i++){
+        for(int j=0; j<n; j++){
+            int s=0;
+            for(int k=i; k<j; k++){
+                s += nums[k];
+            }
+            if(s > m){
+                m = s;
+            }
+        }
+    }
+    return m;
+}
 
+int main() {
+    vector<int> vec = {-2,1,-3,4,-1,2,1,-5,4};
+    cout<<brute_max_arr(vec);
 
-
-int main(){
-    
+    return 0;
 }
