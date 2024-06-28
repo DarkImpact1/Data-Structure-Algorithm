6c3ef142af0f6897c4498035f98bbf84aaf652bf
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-28 18:55:17 +0530

    better sol for max subarray

--- Array/max_subarray.cpp
+++ Array/max_subarray.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
  // time complexity O(n**3)
-int brute_max_arr(vector<int> nums){
+int brute_max_Subarr(vector<int> nums){
     int n=nums.size();
     // because array will contain atleast one element
     int m=nums[0];
@@ -21,9 +21,24 @@ int brute_max_arr(vector<int> nums){
     return m;
 }
 
+
+// time complexity O(n**2)
+int better_max_Subarr(vector<int> nums){
+    int n=nums.size();
+    // because array will contain atleast one element
+    int m=nums[0];
+    for(int i=0;i<n;i++){
+        int s=0;
+        for(int j=0; j<n; j++){
+            if(i == j) continue;
+            s += nums[j];
+            m = max(s,m);
+        }
+    }
+    return m;
+}
 int main() {
     vector<int> vec = {-2,1,-3,4,-1,2,1,-5,4};
-    cout<<brute_max_arr(vec);
-
+    cout<<brute_max_Subarr(vec);
     return 0;
 }
