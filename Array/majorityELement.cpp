97a438695c4f01c4de26459b5bda282bc48d598e
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-28 18:16:21 +0530

    Moore's algorith for majority element

--- Array/majorityELement.cpp
+++ Array/majorityELement.cpp
@@ -32,6 +32,38 @@ int better_find_majority(vector<int> nums){
     }
     return -1;
 }
+
+
+// OPTIMATL SOLTION USING MOORE'S ALGORITHM
+int optimal_find_majority(vector<int> nums){
+    int count=0;
+    int el;
+    for(int i=0; i<nums.size(); i++){
+        if(count == 0){
+            count++;
+            el = nums[i];
+        }else if(el == nums[i]){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    // Since in leetcode problem there will be always an 
+    //element so no need to verify whether it is majority or not
+    int f_count = 0;
+    for(int i : nums){
+        if(i==el){
+            f_count++;
+        }
+    }
+    if(f_count > nums.size()/2){
+        return el;
+    }
+    return -1;
+}
+
+
+
 int main() {
     vector<int> vec ={2,2,1,1,1,2,2};
     cout<<better_find_majority(vec);
