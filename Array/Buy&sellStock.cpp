fd7b576642c7dc692b3c0bcb501b1a6856842be5
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-29 16:14:46 +0530

    buy and sell stock problem

--- Array/Buy&sellStock.cpp
+++ Array/Buy&sellStock.cpp
@@ -1,8 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int sellStock(vector<int> nums){
+    int mini = nums[0], max_profit = 0;
+    for(int i=1; i<nums.size();i++){
+        int cost = nums[i] - mini;
+        max_profit = max(cost,max_profit);
+        mini = min(mini,nums[i]);
+    }
+    return max_profit;
+}
 
 
-int main(){
-    
+int main() {
+    vector<int> prices = {7,6,4,3,1};
+    cout<<sellStock(prices);
+    return 0;
 }
