56328d5d2c7b82a07f63e0f0a5c691aa59301db7
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-29 16:45:12 +0530

    rearrange array

--- Array/RearrangeArray.cpp
+++ Array/RearrangeArray.cpp
@@ -1,13 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+// brute force solution 
 vector<int> rearrangeArray(vector<int>& nums) {
-        
+    vector<int> pos;
+    vector<int> neg;
+    vector<int> f;
+    for(int i: nums){
+        if(i > 0){
+            pos.push_back(i);
+        }else{
+            neg.push_back(i);
+        }        
+    }
+    for(int i=0;i<pos.size();i++){
+        f.push_back(pos[i]);
+        f.push_back(neg[i]);
+    }
+    return f;
     }
 
 
 
 int main(){
-    
+
 }
