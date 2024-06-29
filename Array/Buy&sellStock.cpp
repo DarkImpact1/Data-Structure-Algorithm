8b96d80ab143a3017319ff6e0a92793f58e194b0
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-29 16:28:01 +0530

    buy and sell stock problem

--- Array/Buy&sellStock.cpp
+++ Array/Buy&sellStock.cpp
@@ -5,12 +5,27 @@ int sellStock(vector<int> nums){
     int mini = nums[0], max_profit = 0;
     for(int i=1; i<nums.size();i++){
         int cost = nums[i] - mini;
-        max_profit = max(cost,max_profit);
-        mini = min(mini,nums[i]);
+            max_profit = (cost > max_profit) ? cost : max_profit;
+            mini = (mini < nums[i]) ? mini : nums[i];
     }
     return max_profit;
 }
 
+
+// ANother solution 
+int sellStock1(vector<int> price){
+    int buy=0, sell=1;
+    int profit=0;
+    while(sell < price.size()-1){
+        if(price[sell]- price[buy] < 0){
+            buy = sell;
+        }else{
+            profit = max(profit, price[sell]- price[buy]);
+        }
+        sell++;
+    }
+    return profit;
+}
 
 int main() {
     vector<int> prices = {7,6,4,3,1};
