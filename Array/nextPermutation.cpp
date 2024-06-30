d200f529cca67ddebe616eb51d9879eba66c0f54
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-30 17:44:59 +0530

    next permutation

--- Array/nextPermutation.cpp
+++ Array/nextPermutation.cpp
@@ -14,4 +14,12 @@ vector<int> nextPermutation(vector<int> nums){
         reverse(nums.begin(), nums.end());
         return nums;
     }
+    for(int i = n-1; i>index; i--){
+        if(nums[i] > nums[index]){
+            swap(nums[i], nums[index]);
+            break;
+        }
+    }
+    reverse(nums.begin()+index+1, nums.end());
+    return nums;
 }
