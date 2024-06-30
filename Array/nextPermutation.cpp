7e0a295f79798c349b2c355cc3404a20ba8917d8
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-30 17:56:06 +0530

    next permutation

--- Array/nextPermutation.cpp
+++ Array/nextPermutation.cpp
@@ -1,32 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> nextPermutation(vector<int> nums){
-    int index = -1;
-    int n = nums.size();
-    for(int i= n-2; i>=0; i-- ){
-        if(nums[i] < nums[i+1]){
-            index = i;
-            break;
+void nextPermutation(vector<int>& nums) {
+        int index = -1;
+        int n = nums.size();
+        
+        // Find the first index where nums[i] < nums[i+1] from the end
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
         }
-    }
-    if(index == -1){
-        reverse(nums.begin(), nums.end());
-        return nums;
-    }
-    for(int i = n-1; i>index; i--){
-        if(nums[i] > nums[index]){
-            swap(nums[i], nums[index]);
-            break;
+        
+        // If no such index exists, reverse the entire array
+        if(index == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
         }
+        
+        // Find the smallest number larger than nums[index] to swap with
+        for(int i = n - 1; i > index; i--) {
+            if(nums[i] > nums[index]) {
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+        
+        // Reverse the part of the array after the index
+        reverse(nums.begin() + index + 1, nums.end());
     }
-    reverse(nums.begin()+index+1, nums.end());
-    return nums;
-}
 
 int main() {
     vector<int> vec = {1,2,3};
-    for(int i: findNextPermutation(vec)){
+    findNextPermutation(vec);
+    for(int i: vec){
         cout<<i<<" ";
     }
     return 0;
