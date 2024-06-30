b775c43b0f0fa86e4b3be43e7f64a75bab0e98d5
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-06-30 17:42:33 +0530

    next permutation

--- Array/nextPermutation.cpp
+++ Array/nextPermutation.cpp
@@ -1,2 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+vector<int> nextPermutation(vector<int> nums){
+    int index = -1;
+    int n = nums.size();
+    for(int i= n-2; i>=0; i-- ){
+        if(nums[i] < nums[i+1]){
+            index = i;
+            break;
+        }
+    }
+}
