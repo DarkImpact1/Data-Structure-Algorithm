3089a87a6d3c4c3a9b2d307626839d593e80cb0d
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-07-01 18:12:14 +0530

    longest common subsequence

--- Array/longestSuccesive.cpp
+++ Array/longestSuccesive.cpp
@@ -1,13 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int longestSuccessiveSubseq(vector<int> nums){
-    
+int brute_longestSuccessiveSubseq(vector<int> nums){
+    sort(nums.begin(), nums.end());
+    int last_smaller = INT_MIN;
+    int count = 0;
+    int n = nums.size();
+    int longest = 1;
+    for(int i : nums){
+        if(i-1 == last_smaller){
+
+            count += 1;
+            last_smaller = i;
+        }else if(i != last_smaller){
+            // reassigning and starting counting again
+            count = 1;
+            last_smaller = i;
+        }
+        longest = max(longest, count);
+    }
+    return longest;
 }
-
-
-
 
 int main(){
     vector<int> vec = {1,4,2,5,101,100,99,97,98};
+    cout<<brute_longestSuccessiveSubseq(vec);
 }
