bd98bb9cf63c8787ca7e60983e8f7dd0e9854dee
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-07-01 15:54:24 +0530

    leaders problem

--- Array/leader.cpp
+++ Array/leader.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 
+vector<int> extractLeader(vector<int>& nums){
+    int n =nums.size()-1;
+    int mm = INT_MIN;
+    vector<int> res;
+    for(int i =n; i>=0; i-- ){
+        if(nums[i] > mm){
+            res.push_back(nums[i]);
+            mm = nums[i];
+        }
+    }
+    reverse(res.begin(),res.end());
+    return res;
+}
+
 int main(){
-    vector<int> vec = {}
+    vector<int> vec = {10,22,12,3,0,6};
+    for(int i : extractLeader(vec)){
+        cout<<i <<" ";
+    }
 }
