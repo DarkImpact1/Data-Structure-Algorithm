c5367c775f612f6ca572ad7b8f846d8a60299eaa
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-07-02 14:40:00 +0530

    set matrix zero

--- Array/setMatrixZero.cpp
+++ Array/setMatrixZero.cpp
@@ -2,6 +2,48 @@
 using namespace std;
 
 
+// funtion inside brute_setZero
+void setRow(int row, vector<vector<int>>& matrix){
+    int nC = matrix[0].size();
+    for(int col=0; col<nC; col++){
+        if(matrix[row][col] != 0){
+            matrix[row][col] = -1;
+        }
+    }
+}
+// funtion inside brute_setZero
+void setCol(int col, vector<vector<int>>& matrix){
+    int nR = matrix.size();
+    for(int row=0; row<nR; row++){
+        if(matrix[row][col] != 0){
+            matrix[row][col] = -1;
+        }
+    }
+    
+}
+void brute_setZero(vector<vector<int>>& matrix){
+    int nR = matrix.size();
+    int nC = matrix[0].size();
+    for(int row = 0; row < nR; row++){
+        for(int col = 0; col < nC; col++){
+            if(matrix[row][col] == 0){
+                setRow(row, matrix);// converting all the vales to -1
+                setCol(col,matrix);// converting all the values to -1
+            }
+        }
+    }
+    
+    // now converting -1 to zero
+    
+    for(int row=0; row <nR; row++){
+        for(int col=0; col<nC; col++){
+            if(matrix[row][col] == -1){
+                matrix[row][col]=0;
+            }
+        }
+    }
+    //end
+}
 
 
 
@@ -9,10 +51,9 @@ int main(){
  vector<vector<int>> vec = {
         {1, 1, 1, 1},
         {1, 0, 0, 1},
-        {1, 0, 1, 1}
+        {1, 1, 1, 1}
     };
-    // setZero(vec);
-    
+    brute_setZero(vec);
     for(auto row : vec){
         for(auto ele : row){
             cout<<ele<<" ";
