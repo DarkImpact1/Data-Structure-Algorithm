5c867b55dad7a4c65517403f7907f37a6b99cc15
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-07-02 15:21:40 +0530

    optimal set matrix zero

--- Array/setMatrixZero.cpp
+++ Array/setMatrixZero.cpp
@@ -70,14 +70,62 @@ void better_setZero(vector<vector<int>>& matrix){
     }
 }
 
+
+// optimal without taking extra space .. lets consider our matrix 0th row as hash row and oth columns as hash col 
+void optimal_setZero(vector<vector<int>>& matrix){
+    int nR = matrix.size();
+    int nC = matrix[0].size();
+    //int row[nR]={0};  matrix[0][..]
+    //int col[nC]={0};  matrix[..][0]
+    int col0 = 1;
+    for(int i=0; i<nR; i++){
+        for(int j=0; j<nC; j++){
+            if(matrix[i][j]==0){
+                matrix[i][0] = 0;
+                if(j != 0){
+                    matrix[0][j] = 0;
+                }else{
+                    col0 = 0;
+                }
+            }
+        }
+    }
+// reiterating and setting the zero based on hash array
+
+    for(int i=1; i<nR; i++){
+        for(int j=1; j<nC; j++){
+            if(matrix[i][j] != 0){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+    // setting hash row as 0
+    if(matrix[0][0] == 0){
+        for(int j=0; j<nC; j++){
+            matrix[0][j] = 0;
+        }
+    }
+    // setting hash col as zero
+    if(col0 == 0){
+        for(int i=0; i< nR; i++){
+            matrix[i][0] = 0;
+        }
+    }
+    
+}
+
+
 int main(){
  vector<vector<int>> vec = {
         {1, 1, 1, 1},
-        {1, 0, 0, 1},
+        {1, 1, 0, 1},
         {1, 1, 1, 1}
     };
     // brute_setZero(vec)// around O(n**3)
-    better_setZero(vec); // O(n**2)
+    // better_setZero(vec); // O(n**2) space O(m+n)
+    optimal_setZero(vec);; // O(n**2) space O(i)
     for(auto row : vec){
         for(auto ele : row){
             cout<<ele<<" ";
