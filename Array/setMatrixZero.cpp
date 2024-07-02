e9bca2f05dfb1f92c1e17dced2a7a07f35a6492a
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-07-02 14:23:43 +0530

    set matrix zero

--- Array/setMatrixZero.cpp
+++ Array/setMatrixZero.cpp
@@ -6,5 +6,17 @@ using namespace std;
 
 
 int main(){
+ vector<vector<int>> vec = {
+        {1, 1, 1, 1},
+        {1, 0, 0, 1},
+        {1, 0, 1, 1}
+    };
+    // setZero(vec);
     
+    for(auto row : vec){
+        for(auto ele : row){
+            cout<<ele<<" ";
+        }
+        cout<<endl;
+    }
 }
