1345f6e1c2af9631cd73d45321f32dbdc4ddd89b
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-07-03 17:15:23 +0530

    print spiral matrix

--- Array/printSpiral.cpp
+++ Array/printSpiral.cpp
@@ -3,11 +3,36 @@ using namespace std;
 
 
 vector<int> printSpiral(vector<vector<int>>& matrix){
-    int nR = matrix.size();
-    int nC = matrix[0].size();
-    for(int i = 0; i<nR; i++){
-        
+    int left = 0, top = 0;
+    int right = matrix[0].size() - 1, bottom = matrix.size() - 1;
+    vector<int> ans;
+
+    while (top <= bottom && left <= right) {
+        for (int k = left; k <= right; k++) {
+            ans.push_back(matrix[top][k]);
+        }
+        top += 1;
+
+        for (int k = top; k <= bottom; k++) {
+            ans.push_back(matrix[k][right]);
+        }
+        right -= 1;
+
+        if (top <= bottom) {
+            for (int k = right; k >= left; k--) {
+                ans.push_back(matrix[bottom][k]);
+            }
+            bottom -= 1;
+        }
+
+        if (left <= right) {
+            for (int k = bottom; k >= top; k--) {
+                ans.push_back(matrix[k][left]);
+            }
+            left += 1;
+        }
     }
+    return ans;
 }
 
 int main(){
