0e7ae0951dbe9c3ba655330ba8626127966528f8
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-07-05 15:45:19 +0530

    brute four sum

--- Array/foursum.cpp
+++ Array/foursum.cpp
@@ -25,10 +25,34 @@ vector<vector<int>> fourSum(vector<int> nums, int target){
     return result;
 }
 
+// brute force using three for loop for fourth we'll go fourth = target - nums[i]+ nums[j]+ nums[k]
+vector<vector<int>> better_fourSum(vector<int> nums, int target){
+    set<vector<int>> st;
+    int n = nums.size();
+    for(int i = 0 ; i< n; i++){
+        for(int j = i+1; j <n ; j++){
+            set<int> hashset;
+            for(int k = j+1; k < n; k++){
+                    int sm = nums[i] + nums[j];
+                    sm += nums[k];
+                    int fourth = target - sm;
+                    if(hashset.find(fourth) != hashset.end()){
+                        vector<int> temp = {nums[i], nums[j],nums[k],fourth};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                    hashset.insert(nums[k]);
+            }
+        }
+    }
+    vector<vector<int>> result(st.begin(), st.end());
+    return result;
+}
+
 int main(){
     vector<int> vec = {1,0,-1,0,-2,2};
     int target = 0;
-    vector<vector<int>> res = fourSum(vec,target);
+    vector<vector<int>> res = better_fourSum(vec,target);
     for(auto array : res){
         for(auto item : array){
             cout<<item << " ";
