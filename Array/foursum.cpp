49abbb24d661749d847de92489470054c6984913
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-07-05 15:34:55 +0530

    brute four sum

--- Array/foursum.cpp
+++ Array/foursum.cpp
@@ -1,9 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+// brute force using four for loop 
+vector<vector<int>> fourSum(vector<int> nums, int target){
+    set<vector<int>> st;
+    int n = nums.size();
+    for(int i = 0 ; i< n; i++){
+        for(int j = i+1; j <n ; j++){
+            for(int k = j+1; k < n; k++){
+                for(int l = k+1; l<n; l++){
+                    int sm = nums[i] + nums[j];
+                    sm += nums[k];
+                    sm += nums[l];
+                    if(sm == target){
+                        vector<int> temp = {nums[i], nums[j],nums[k],nums[l]};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+    }
+    vector<vector<int>> result(st.begin(), st.end());
+    return result;
+}
 
 int main(){
-    vector<int> vec = {2,4,3,5,6,8};
+    vector<int> vec = {1,0,-1,0,-2,2};
+    int target = 0;
+    vector<vector<int>> res = fourSum(vec,target);
+    for(auto array : res){
+        for(auto item : array){
+            cout<<item << " ";
+        }
+        cout<<endl;
+    }
         
 }
