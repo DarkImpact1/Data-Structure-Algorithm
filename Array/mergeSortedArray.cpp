f93bf4ce39560a0c22184bd00317bca99c92658e
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-07-06 19:41:50 +0530

    Update mergeSortedArray.cpp

--- Array/mergeSortedArray.cpp
+++ Array/mergeSortedArray.cpp
@@ -20,7 +20,23 @@ void brute_merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
         nums1[i] = svector[i];
     }
 }
-void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
+void optimal1_merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
+    int left = m-1;
+    int right = 0;
+    while(left >=0 && right < n){
+        if(nums1[left] > nums2[right]){
+            swap(nums1[left], nums2[right]);
+            left--;
+            right++;
+        }else{
+            break;
+        }
+    }
+    sort(nums1.begin(), nums1.begin() + m);
+    sort(nums2.begin(), nums2.end());
+    for(int i=0; i<n;i++){
+        nums1[m+i] = nums2[i];
+    }
     
 }
 int main() {
@@ -28,7 +44,7 @@ int main() {
     vector<int> vec2 = {2,5,6,7};
     int m = 3;
     int n = 4;
-    brute_merge(vec1,m,vec2,n);
+    optimal1_merge(vec1,m,vec2,n);
     for(auto it : vec1){
         cout<<it <<" ";
     }
