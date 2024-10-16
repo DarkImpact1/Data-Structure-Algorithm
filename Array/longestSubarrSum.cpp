8ed8719947f50c074ccc4a98ea5085fc021ed619
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2024-10-17 01:09:36 +0530

    Update longestSubarrSum.cpp

--- Array/longestSubarrSum.cpp
+++ Array/longestSubarrSum.cpp
@@ -23,8 +23,31 @@ int maxSubarraySum(vector<int> nums, int k){
     }
     return maxlen-1;
 }
+/*
+Problem max length of subarrya which can yield sum <= k
+    */
+int optimalLongestSubrrayK(vector<int> &arr, int k){
+    int l = 0, r = 0, n = arr.size(), sum = 0, maxlen = 0;
+    while(r < n){
+        sum = sum + arr[r];
+        while(sum > k && (r-l+1) > maxlen){
+            sum = sum - arr[l];
+            l = l+1;
+        }
+        if(sum <= k){
+            maxlen = max(maxlen,r-l+1);
+        }
+        r = r+1;
+    }
+    return maxlen;
+}
 
 int main() {
+    string name = "babad";
+    vector<int> arr = {12,8,1,7,10};
+    int length = optimalLongestSubrrayK(arr, 11);
+    cout<<"Max lenght of subarray with sum <= "<<k<<" : "<<length<<endl;
+    
     vector<int> vec  = {0,1,1,1,1,1,1};
     int k = 6;
     cout<<"Max lenght of subarray with sum "<<k<<" : "<<maxSubarraySum(vec,k);
