7e81dea69d5fd73eb8a425758f84d7ba3d130a23
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2025-01-15 01:08:24 +0530

    Create leetcode2147.cpp

---
+++ Leetcode/leetcode2147.cpp
@@ -0,0 +1,43 @@
+// Description:
+/*
+2147. Number of Ways to Divide a Long Corridor
+Along a long library corridor, there is a line of seats and decorative plants.
+You are given a 0-indexed string corridor of length n consisting of letters 'S' and 'P' 
+where each 'S' represents a seat and each 'P' represents a plant.
+One room divider has already been installed to the left of index 0, and another to the 
+right of index n - 1. Additional room dividers can be installed. For each position between 
+indices i - 1 and i (1 <= i <= n - 1), at most one divider can be installed.
+Divide the corridor into non-overlapping sections, where each section has exactly two seats 
+with any number of plants. There may be multiple ways to perform the division. Two ways are different 
+if there is a position with a room divider installed in the first way but not in the second way.
+Return the number of ways to divide the corridor. Since the answer may be very large,
+return it modulo 109 + 7. If there is no way, return 0.
+*/
+
+// Solution 
+
+int numberOfWays(string corridor) {
+    int n = corridor.length();
+    long m = 1000000007, ways = 1;
+    int i=0, j=0,cnt_seat = 0,total_seat =0;
+    while(j < n){
+        if(corridor[j]=='S'){
+            cnt_seat++;
+            total_seat++;
+            
+            if(cnt_seat == 2){
+                i = j;
+            }
+            else if (cnt_seat == 3){
+                ways = ((ways%m) * ((j-i)%m))%m;
+                cnt_seat = 1;
+            }
+        }
+        j++;
+    }
+    if(total_seat == 0 || total_seat%2 !=0){
+        return 0;
+    }
+    
+    return ways;
+}
