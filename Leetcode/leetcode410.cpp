0c9112631da3a9e4fbf01f6f2e315c1cb1a84a69
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2025-01-25 10:31:26 +0530

    Create leetcode410.cpp

---
+++ Leetcode/leetcode410.cpp
@@ -0,0 +1,13 @@
+/*
+410. Split Array Largest Sum
+Hard
+Topics
+Companies
+Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
+
+Return the minimized largest sum of the split.
+
+A subarray is a contiguous part of the array.
+
+
+
