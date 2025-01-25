07b6de884e060bf3fa2e7ce39950905d786c1019
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2025-01-25 11:31:02 +0530

    Update leetcode410.cpp

--- Leetcode/leetcode410.cpp
+++ Leetcode/leetcode410.cpp
@@ -9,5 +9,42 @@ Return the minimized largest sum of the split.
 
 A subarray is a contiguous part of the array.
 
+*/
 
+// THIS PROBLEM IS SAME AS THE BOOK ALLOCATION AND PAINTER'S PROBLEM SO MAKE SURE YOU REVISIST BOTH PROBLEM AND REVISE THE CONCEPT
+class Solution {
+public:
+    void updateLowHigh(vector<int>& nums, int &low, int &high){
+        int s = 0;
+        for(auto n : nums){
+            if( n > low)  low = n;
+            s+= n;
+        }
+        high = s;
+    }
+
+    int numSubArr(vector<int> & nums, int minSum){
+        int numSub = 1, add = 0;
+        for( auto n : nums){
+            if ( (add + n) > minSum){
+                    numSub++;
+                    add = n;
+            }else{
+                add += n;
+            }
+        }
+        return numSub;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int low = INT_MIN, high = 0;
+        updateLowHigh(nums, low, high);
+        while(low <= high){
+            int midSum = (low+high)/2;
+            int numSubPossible = numSubArr(nums, midSum);
+            if(numSubPossible > k) low = midSum+1;
+            else high = midSum -1;
+        }
+        return low;
+    }
+};
 
