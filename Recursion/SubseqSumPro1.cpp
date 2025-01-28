78fbf0e08afeb91302a7ccd326c48d0354a752f5
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2025-01-28 21:52:52 +0530

    Create SubseqSumPro1.cpp

---
+++ Recursion/SubseqSumPro1.cpp
@@ -0,0 +1,45 @@
+// number of subsequence that can yeild sum <= target
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findSum(vector<int>& nums){
+    if(nums.size() == 0)return 0;
+    if(nums.size() == 1) return nums[0];
+    int mini = INT_MAX, maxi=INT_MIN;
+    for(auto it: nums){
+        if(it < mini) mini = it;
+        if(it > maxi) maxi = it;
+    }
+    return mini +  maxi;
+}
+void generateSub(int i,vector<int>&nums, vector<int> temp,int &count, int target){
+    
+    if(i == nums.size()){
+        int s = findSum(temp);
+        if(s <= target){
+            count++;
+        }
+        return;
+    }
+    
+    temp.push_back(nums[i]);
+    generateSub(i+1,nums,temp,count,target);
+    
+    temp.pop_back();
+    generateSub(i+1,nums, temp,count,target);
+}
+int numSubseq(vector<int>& nums, int target) {
+    int i=0, count=0;
+    vector<int> temp;
+    generateSub(i,nums,temp,count,target);
+    return count-1;
+}
+
+int main() {
+    vector<int> nums = {3,5,6,7};
+    cout<<numSubseq(nums, 9);
+    
+    return 0;
+}
