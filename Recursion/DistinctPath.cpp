ec5b88de823c210638ea3dac61e02394b55762bd
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2025-02-01 02:49:15 +0530

    Update DistinctPath.cpp

--- Recursion/DistinctPath.cpp
+++ Recursion/DistinctPath.cpp
@@ -1,31 +1,38 @@
 // given a row x col grid if a user can move either downward or either rightward count the number of distinct path that is available
-
-
 // Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
 using namespace std;
 
 int countDistinctPath(int row, int col, int i, int j){
-  // if reaches the end return 1
     if(i== row-1 && j == col-1){
         return 1;
     }
-    // if user is at the very bottm of grid then he can only move rightward 
     if(i == row-1){
         return countDistinctPath(row, col, i, j+1);
     }
-  // if user is at the right corner then he can only move downward only
     if(j == col-1){
         return countDistinctPath(row, col, i+1, j);
     }
-  // if user is somewhere between then he can either move downward or rightward
     if(i < row && j < col){
         return countDistinctPath(row, col, i+1, j)+ countDistinctPath(row, col, i, j+1);
     }
-  // else return 0
     return 0;
 }
+
+// optimised one
+void optimisedCount(int row, int col, int i, int j, int& count){
+    // if you are the last row or last col simply increase the count becuase there will be only one path 
+    if( i == row -1 || j == col-1){
+        count++;
+        return;
+    }
+    optimisedCount(row, col, i+1, j, count);
+    optimisedCount(row, col, i, j+1, count);
+}
 int main() {
-    cout<<countDistinctPath(3,3,1,1);
+    cout<<countDistinctPath(3,3,0,0)<<endl;
+    int count = 0;
+    optimisedCount(3,3,0,0,count);
+    cout<<count<<endl;
     return 0;
 }
